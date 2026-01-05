8c767dd64840f5717142a3cc7dc9f08dcfc19b1b
drkameleon <yaniszaf@gmail.com>
2026-01-05 12:32:43 +0100

    add nice log messages

--- scripts/process.rb
+++ scripts/process.rb
@@ -1,24 +1,43 @@
+require 'colorize'
+
 script_dir = __dir__
 root_dir = File.expand_path("..", script_dir)
 
-puts "- Compressing complete.json"
+def header(title)
+    puts "----------------------------------------------------------------------".bold.cyan
+    puts title.bold.cyan
+    puts "----------------------------------------------------------------------".bold.cyan
+end
+
+def success 
+    puts " [ ✔ OK ]".green
+end
+
+header("Processing main dataset")
+print "- Compressing list...       "
 `ruby #{script_dir}/minify.rb #{root_dir}/complete.json`
+success()
 
 [
     ["old", ["1","2","3","4","5","6"]],
-    ["new", ["1","2","3","4","5","6","7+"]]
+    ["new", ["1","2","3","4","5","6","7"]]
 ].each{|scheme, levels|
     cumulative = []
-    
+    puts "\n"
+    header("Processing #{scheme} levels")
     levels.each{|x|
-        puts "- Creating sublist for #{scheme} level: #{x}..."
+        puts "\tLevel: #{x}".yellow
+        print "\t\t- Filtering exclusive list  "
         `ruby #{script_dir}/filter.rb exclusive #{scheme}-#{x}`
+        success()
         cumulative << "#{scheme}-#{x}"
-        puts "Calling: ruby #{script_dir}/filter.rb inclusive #{cumulative.join(" ")}"
+        print "\t\t- Filtering inclusive list  "
         `ruby #{script_dir}/filter.rb inclusive #{cumulative.join(" ")}`
+        success()
 
-        puts "\t - Compressing..."
+        print "\t\t- Compressing lists...      "
         `ruby #{script_dir}/minify.rb #{root_dir}/wordlists/exclusive/#{scheme}/#{x}.json`
         `ruby #{script_dir}/minify.rb #{root_dir}/wordlists/inclusive/#{scheme}/#{x}.json`
+        success()
     }
 }
